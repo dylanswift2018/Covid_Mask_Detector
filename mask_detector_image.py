b199abcb45b4a3fbb0a468b674db57257221a83e
Lotfi <hl_feraga@esi.dz>
2021-07-17 10:16:34 +0100

    some test images

--- mask_detector_image.py
+++ mask_detector_image.py
@@ -9,3 +9,14 @@ import numpy as np
 import argparse
 import cv2
 import os
+
+# arguments parser 
+arg = argparse.ArgumentParser()
+arg.add_argument("-i", "--image", required=True,help="path to input image")
+arg.add_argument("-f", "--face", type=str,default="face_detector",help="path to face detector model directory")
+arg.add_argument("-m", "--model", type=str,default="mask_detector.model",help="path to trained face mask detector model")
+arg.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+args = vars(arg.parse_args())
+
+
