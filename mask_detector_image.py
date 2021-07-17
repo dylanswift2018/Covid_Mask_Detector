ebd37000b9c480fb97beee9ceff90c10fd2658d6
Lotfi <hl_feraga@esi.dz>
2021-07-17 10:11:33 +0100

    static detector in

--- mask_detector_image.py
+++ mask_detector_image.py
@@ -1,2 +1,11 @@
 # how to run the detector 
 # py mask_detector_image.py --image  test/image.png
+
+#imports 
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import argparse
+import cv2
+import os
